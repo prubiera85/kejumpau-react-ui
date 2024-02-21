c09f976d8d5f65a0ad285fda4917faa89b18b53b
prubiera85 <paula.rubiera@oneclick.es>
2024-02-21 15:42:21 +0100

    Add test button

---
+++ components/testButton/TestButton.jsx
@@ -0,0 +1,9 @@
+import './_testButton.scss'
+
+const TestButton = () => {
+  return (
+    <div className="test-button">This is a test button from the submodule</div>
+  );
+};
+
+export default TestButton;
